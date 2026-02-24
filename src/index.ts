c1f0ff27c9a1f093ae003b79b769a9a268908e75
Mehdi Nabhani <mehdi@nabhani.de>
2026-02-24 17:19:12 +0100

    Initial commit from create-minions-bundle scaffolder

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Minions Bundle: Finance
+ *
+ * Financial tracking bundle — budgets, expenses, invoices
+ *
+ * @module minions-bundles-finance
+ */
+
+export const VERSION = '0.1.0';
+
+export * from './bundle.js';
+export * from './relations.js';
+export * from './views.js';
